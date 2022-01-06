681c0887a5622f3b43189726024e58424391e137
Ankur Rohilla <ankur.rohilla1996@gmail.com>
2022-01-06 09:45:07 +0530

    update setting for https

--- backend/settings.py
+++ backend/settings.py
@@ -82,6 +82,14 @@ if DEBUG:
 else:
     DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
     ALLOWED_HOSTS = ['.herokuapp.com']
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+    SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+    # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+    SESSION_COOKIE_SECURE = True
+    # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+    CSRF_COOKIE_SECURE = True
 
 
 # Password validation
